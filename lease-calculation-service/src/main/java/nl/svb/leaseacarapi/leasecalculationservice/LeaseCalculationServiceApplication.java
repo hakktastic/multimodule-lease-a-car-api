cf7943ee3a3d75b25da74a536d41cdd212b641db
hakktastic <49901606+hakktastic@users.noreply.github.com>
2021-01-31 09:29:12 +0100

    Re-enabled Sleuth
    Re-enabled Spring Cloud Sleuth for tracing with Google Stackdriver.

--- lease-calculation-service/src/main/java/nl/svb/leaseacarapi/leasecalculationservice/LeaseCalculationServiceApplication.java
+++ lease-calculation-service/src/main/java/nl/svb/leaseacarapi/leasecalculationservice/LeaseCalculationServiceApplication.java
@@ -1,12 +1,12 @@
 package nl.svb.leaseacarapi.leasecalculationservice;
 
-// import brave.sampler.Sampler;
+import brave.sampler.Sampler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-// import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableFeignClients("nl.svb.leaseacarapi.leasecalculationservice")
@@ -23,10 +23,10 @@ public class LeaseCalculationServiceApplication {
    *
    * @return Returns a Sampler object
    */
-  // @Bean
-  // public Sampler defaultSampler() {
-  //
-  // // TODO implement Sleuth for only one request
-  // return Sampler.ALWAYS_SAMPLE;
-  // }
+  @Bean
+  public Sampler defaultSampler() {
+
+    // TODO implement Sleuth for only one request
+    return Sampler.ALWAYS_SAMPLE;
+  }
 }
